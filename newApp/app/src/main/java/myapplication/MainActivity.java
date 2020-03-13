c132b7dc15a3c2d97a01894f78c92f3579a7dc52
sinpulse <spedits@gmail.com>
2020-03-13 02:26:31 -0600

    user input point displays

--- newApp/app/src/main/java/myapplication/MainActivity.java
+++ newApp/app/src/main/java/myapplication/MainActivity.java
@@ -9,7 +9,9 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import myapplication.R;
 
-import java.util.Vector;
+//import java.util.Vector;
+import java.lang.*;
+import java.util.*;
 
 //import android.support.v7.app.AppCompatActivity;
 
@@ -17,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     double tempPointx;
     double tempPointy;
-    double outputPoint;
+    String outputPointx;
+    String outputPointy;
 
     EditText xCoordinateInput;
     EditText yCoordinateInput;
@@ -30,13 +33,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //**************** TEST VALUES
-        coordObj.coordinates clusterT = new coordObj.coordinates();
-        clusterT.x = new Vector();
-        clusterT.y = new Vector();
+        //**************** SINGLE CLUSTER
+        //.coordinates clusterT = new coordObj.coordinates();
+        //clusterT.x = new Vector();
+        //clusterT.y = new Vector();
 
-        clusterT.x.add(Double.parseDouble("5.0"));
-        clusterT.y.add(Double.parseDouble("6.0"));
+        //clusterT.x.add(Double.parseDouble("5.0"));
+        //clusterT.y.add(Double.parseDouble("6.0"));
 
         //****************
 
@@ -49,26 +52,43 @@ public class MainActivity extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                coordObj.coordinates clusterT = new coordObj.coordinates();
+                clusterT.x = new Vector();
+                clusterT.y = new Vector();
 
                 //If statements to avoid app crash if submit button is pressed without user input
                 if(xCoordinateInput.getText().toString().length() == 0)
                 {
+
                     xCoordinateInput.setText("0");
                 }
                 if(yCoordinateInput.getText().toString().length() == 0)
                 {
                     yCoordinateInput.setText("0");
+
                 }
                 tempPointx = Double.parseDouble(xCoordinateInput.getText().toString());
                 tempPointy = Double.parseDouble(yCoordinateInput.getText().toString());
-                outputPoint = tempPointx + tempPointy;
+                clusterT.x.add(tempPointx);
+                clusterT.y.add(tempPointy);
 
-                //uh
-                outputNum.setText(String.valueOf(coordObj.coordinates.x.get(0)));
+                //OUTPUT:
+                outputPointx = String.valueOf(clusterT.x.get(0));
+                outputPointy = String.valueOf(clusterT.y.get(0));
+                outputNum.setText(String.format("(%s,%s)", outputPointx, outputPointy));
 
-                double testD = parserFile.parser.tester(600.00);
+                //outputNum.setText(String.valueOf(clusterT.x.get(0)));//dead
+                //outputNum.setText(String.valueOf(outputPoint));//dead
+                //double testD = parserFile.parser.tester(600.00);//dead
+
+
+
+
+
+
+
                 //debug call&
-                showToast(String.valueOf(testD));//dead
+                //showToast(String.valueOf(testD));
             }
         });
     }
