eb7a77975ba1a36906a74e84e19a98e35471b0cb
sinpulse <spedits@gmail.com>
2020-03-12 23:53:49 -0600

    so it currently adds numbers

--- newApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ newApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,18 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     String name, email;
-    double xCoordinate;
-    double yCoordinate;
+    double tempPointx;
+    double tempPointy;
+    double outputPoint;
 
-    EditText nameInput;
-    EditText emailInput;
     EditText xCoordinateInput;
     EditText yCoordinateInput;
+    TextView outputNum;
 
     Button submitButton;
 
@@ -26,28 +27,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //nameInput = (EditText) findViewById(R.id.nameInput);
-        //emailInput = (EditText) findViewById(R.id.emailInput);
         xCoordinateInput = (EditText) findViewById(R.id.xCoordinateInput);
         yCoordinateInput = (EditText) findViewById(R.id.yCoordinateInput);
+        outputNum = (TextView) findViewById(R.id.outputNum);
 
+        //SubmitButton
         submitButton = (Button) findViewById(R.id.submitButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //name = nameInput.getText().toString();
-                //email = emailInput.getText().toString();
-                xCoordinate = Double.valueOf(xCoordinateInput.getText().toString());
-                yCoordinate = Double.valueOf(yCoordinateInput.getText().toString());
 
-                //showToast(name);
-                //showToast(email);
-                //showToast(String.valueOf(xCoordinate));
-                //showToast(String.valueOf(yCoordinate));
-                showToast(String.valueOf(xCoordinate + yCoordinate));
+                //If statements to avoid app crash if submit button is pressed without user input
+                if(xCoordinateInput.getText().toString().length() == 0)
+                {
+                    xCoordinateInput.setText("0");
+                }
+                if(yCoordinateInput.getText().toString().length() == 0)
+                {
+                    yCoordinateInput.setText("0");
+                }
+                tempPointx = Double.parseDouble(xCoordinateInput.getText().toString());
+                tempPointy = Double.parseDouble(yCoordinateInput.getText().toString());
+                outputPoint = tempPointx + tempPointy;
+                outputNum.setText(String.valueOf(outputPoint));
+
+                //debug call&
+                //showToast(String.valueOf(tempPoint));//dead
             }
         });
     }
+    //debug func&
     private void showToast(String text) {
         Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
     }
