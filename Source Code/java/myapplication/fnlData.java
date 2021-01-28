8220882247c091e3edf6134b50c25bf5282a5551
sinpulse <spedits@gmail.com>
2021-01-27 22:12:52 -0700

    re clang format

--- Source Code/java/myapplication/fnlData.java
+++ Source Code/java/myapplication/fnlData.java
@@ -1,30 +1,27 @@
 package myapplication;
-//package fnlD;
+// package fnlD;
 /*
 
 */
-//Headers:
+// Headers:
 import java.lang.*;
 import java.util.*;
 
 @SuppressWarnings("unchecked")
 
-public class fnlData
-{
-  public Vector<Double> lx;
-  public Vector<Double> ly;
-  public Vector<Double> hx;
-  public Vector<Double> hy;
-  public Vector<Double> id;
-  public fnlData(Vector<Double> lx, Vector<Double> ly, Vector<Double> hx, Vector<Double> hy, Vector<Double> id)
-  {
-    this.lx = lx;
-    this.ly = ly;
-    this.hx = hx;
-    this.hy = hy;
-    this.id = id;
-  }
-  public fnlData()
-  {
-  }
+public class fnlData {
+    public Vector<Double> lx;
+    public Vector<Double> ly;
+    public Vector<Double> hx;
+    public Vector<Double> hy;
+    public Vector<Double> id;
+    public fnlData(Vector<Double> lx, Vector<Double> ly, Vector<Double> hx, Vector<Double> hy,
+        Vector<Double> id) {
+        this.lx = lx;
+        this.ly = ly;
+        this.hx = hx;
+        this.hy = hy;
+        this.id = id;
+    }
+    public fnlData() {}
 }
